6e377956e8c483063b884738e235c525d314a044
ovi13 <prothom.ovi@gmail.com>
2025-07-29 23:19:53 -0700

    Update: optimized homeView.js layout and user interactions

--- js/views/homeView.js
+++ js/views/homeView.js
@@ -1,11 +1,12 @@
 // js/views/homeView.js
 
 // Function to render the home page content
-function renderHomeView(targetElement) {
+window.renderHomeView = function(targetElement) { // <<< ADDED window.
     const homeHtml = `
         <h1>Welcome to the Utility Bill Section</h1>
         <p class="tagline">Simplifying bill management for everyone</p>
         <p class="credit">Created by Prothom Dey Ovi</p>
     `;
     targetElement.innerHTML = homeHtml;
+    console.log('homeView.js: Home page rendered.'); // Debugging
 }
